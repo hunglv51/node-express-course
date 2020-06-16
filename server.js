a121f52d4ead34aa7e3017060ad23195f4918bf1
hunglv51 <hung561992@gmail.com>
2020-06-16 10:30:45 +0700

    add login post route

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const bodyParser = require('body_parser');
 const app = express();
-
+app.use(bodyParser.json());
 const mockUserData = [
     {name: "Tom"},
     {name: "Jerry"},
 ];
 
-app.get('/users/:id', (req, res) => {
+app.get('/users', (req, res) => {
     console.log(req.params.id);
     res.json({
         success: true,
@@ -14,6 +15,36 @@ app.get('/users/:id', (req, res) => {
         users: mockUserData
     });
 })
+
+app.get('/users/:id', (req, res) => {
+    console.log(req.params.id);
+    res.json({
+        success: true,
+        message: "received user",
+        data: req.params.id
+    });
+})
+
+app.post('login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = 'tom';
+    const mockPassword = 'notjerry';
+    if(username === mockUsername && password === mockPassword){
+        res.json({
+            success: true,
+            message: 'login successfully',
+            token: 'random token'
+        });
+    }
+    else{
+        res.json({
+            success: false,
+            message: 'login failed'
+        })
+    }
+});
 
 app.listen(8080, () => {
     console.log('server is running');
