3fdc3c5ef6b6fad00be364310fa3f8339b4bb413
hunglv51 <hung561992@gmail.com>
2020-06-16 10:18:49 +0700

    add first get route

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const app = express();
 app.listen(8080, () => {
     console.log('server is running');
 });
+
+const mockUserData = [
+    {name: "Tom"},
+    {name: "Jerry"},
+];
+
+app.get('/users', (req, res) => {
+    res.json({
+        success: true,
+        message: "received list users",
+        users: mockUserData
+    });
+})
